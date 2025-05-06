6e9ff8b43c65685b416969b3082cdf9cc5364317
Matúš Ollah <matusollah420@gmail.com>
2025-05-06 13:10:46 +0200

    update GoDoc

--- ust/ust.go
+++ ust/ust.go
@@ -1,2 +1,4 @@
-// Package ust implements encoding and decoding of UST (UTAU Sequence Text).
+// Package ust implements encoding and decoding of UST (UTAU Sequence Text) as implemented in [libutaust].
+//
+// [libutaust]: https://github.com/Sleepwalking/libutaust
 package ust
