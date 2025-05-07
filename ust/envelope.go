872fe8d92528ef5adfecc25e8dc467d836f0ad77
Matúš Ollah <matusollah420@gmail.com>
2025-05-07 20:24:05 +0200

    move envelope to seperate file

---
+++ ust/envelope.go
@@ -0,0 +1,13 @@
+package ust
+
+// Envelope represents a volume envelope.
+type Envelope struct {
+	P1    float64
+	P2    float64
+	P3    float64
+	V1    float64
+	V2    float64
+	V3    float64
+	V4    float64
+	Extra []float64
+}
--- ust/note.go
+++ ust/note.go
@@ -4,18 +4,6 @@ import (
 	"gitlab.com/gomidi/midi/v2"
 )
 
-// Envelope represents a volume envelope.
-type Envelope struct {
-	P1    float64
-	P2    float64
-	P3    float64
-	V1    float64
-	V2    float64
-	V3    float64
-	V4    float64
-	Extra []float64
-}
-
 // Note represents a note in an UST file.
 type Note struct {
 	Length       int       // Length is the duration in ticks.
