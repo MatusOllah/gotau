a020c9cad819d9eed5b1732eadd35eea0fa4fdea
Matúš Ollah <matusollah420@gmail.com>
2025-05-08 11:30:12 +0200

    add Notes field

--- ust/file.go
+++ ust/file.go
@@ -8,6 +8,7 @@ import (
 type File struct {
 	Version  Version  // Version is the UST file format version.
 	Settings Settings // Settings represents the settings of the UST file.
+	Notes    []Note   // Notes hold the notes.
 
 	iniFile *ini.File // iniFile holds the raw parsed INI file structure (used internally).
 }
