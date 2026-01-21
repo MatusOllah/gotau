fe80d3fc4214f518c45b89ef57a8bd00e92f81da
Matúš Ollah <57183251+MatusOllah@users.noreply.github.com>
2026-01-21 12:34:09 +0100

    WIP...

--- cmd/gotau/main.go
+++ cmd/gotau/main.go
@@ -5,46 +5,13 @@ import (
 	"os"
 
 	"github.com/MatusOllah/enczip/zip"
+	"github.com/davecgh/go-spew/spew"
 
 	"github.com/MatusOllah/gotau/voicebank"
-	"github.com/davecgh/go-spew/spew"
 	"golang.org/x/text/encoding/japanese"
 )
 
 func main() {
-	/*
-			s := `[#VERSION]
-		UST Version1.2
-		[#SETTING]
-		Tempo=120
-		Tracks=1
-		Project=C:\Users\matus\Desktop\test.ust
-		VoiceDir=C:\Users\matus\Documents\OpenUtau\Singers\重音テト OU用日本語統合ライブラリー
-		CacheDir=C:\Users\matus\Documents\OpenUtau\Cache
-		Mode2=True
-		[#0000]
-		Length=720
-		Lyric=a
-		NoteNum=69
-		PreUtterance=
-		Velocity=100
-		Intensity=100
-		Modulation=0
-		Flags=g0B0H0P86
-		PBS=-40;0
-		PBW=65
-		PBY=0
-		PBM=,
-		[#TRACKEND]`
-
-			f, err := ust.Decode(strings.NewReader(s))
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Printf("f: %+v\n", f)
-	*/
-
 	zr, err := zip.OpenReader(os.Args[1], japanese.ShiftJIS)
 	if err != nil {
 		panic(err)
@@ -60,7 +27,7 @@ func main() {
 		panic(err)
 	}
 
-	//spew.Dump(vb)
+	spew.Dump(vb)
 
 	// render image
 	if vb.CharacterInfo.Image.Image != nil {
@@ -86,9 +53,4 @@ func main() {
 
 	// print readme
 	fmt.Println(vb.Readme)
-
-	// print prefix.map
-
-	fmt.Println()
-	spew.Dump(vb.PrefixMap)
 }
