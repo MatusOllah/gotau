bcf0ddd20e3063e67da384cfe3e62b7682a907a2
Matúš Ollah <57183251+MatusOllah@users.noreply.github.com>
2026-01-30 11:20:36 +0100

    Add prefix.map unit tests

---
+++ voicebank/prefixmap_test.go
@@ -0,0 +1,114 @@
+package voicebank_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/MatusOllah/gotau/voicebank"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/gomidi/midi/v2"
+	"golang.org/x/text/encoding/japanese"
+)
+
+func TestDecodePrefixMap(t *testing.T) {
+	s := "C5\tpre\tsuf\nD#5\tpre\t\n# comment\nF#5\t\tsuf\n"
+
+	want := voicebank.PrefixMap{
+		midi.Note(midi.C(5)):  voicebank.Prefix{"pre", "suf"},
+		midi.Note(midi.Eb(5)): voicebank.Prefix{"pre", ""},
+		midi.Note(midi.Gb(5)): voicebank.Prefix{"", "suf"},
+	}
+
+	prefixmap, err := voicebank.DecodePrefixMap(strings.NewReader(s))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, want, prefixmap)
+}
+
+func TestDecodePrefixMapWithOpts(t *testing.T) {
+	s := "C5 pre suf\nD#5 pre \n; comment\nF#5  suf\n"
+
+	want := voicebank.PrefixMap{
+		midi.Note(midi.C(5)):  voicebank.Prefix{"pre", "suf"},
+		midi.Note(midi.Eb(5)): voicebank.Prefix{"pre", ""},
+		midi.Note(midi.Gb(5)): voicebank.Prefix{"", "suf"},
+	}
+
+	prefixmap, err := voicebank.DecodePrefixMap(strings.NewReader(s),
+		voicebank.PrefixMapWithEncoding(japanese.ShiftJIS),
+		voicebank.PrefixMapWithComment(';'),
+		voicebank.PrefixMapWithDelimiter(' '),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, want, prefixmap)
+}
+
+func TestDecodePrefixMapAllNotes(t *testing.T) {
+	s := "C5\t\tfoo\n" +
+		"C#5\t\tfoo\n" +
+		"Db5\t\tfoo\n" +
+		"D5\t\tfoo\n" +
+		"D#5\t\tfoo\n" +
+		"Eb5\t\tfoo\n" +
+		"E5\t\tfoo\n" +
+		"F5\t\tfoo\n" +
+		"F#5\t\tfoo\n" +
+		"Gb5\t\tfoo\n" +
+		"G5\t\tfoo\n" +
+		"G#5\t\tfoo\n" +
+		"Ab5\t\tfoo\n" +
+		"A5\t\tfoo\n" +
+		"A#5\t\tfoo\n" +
+		"Bb5\t\tfoo\n" +
+		"Hb5\t\tfoo\n" +
+		"B5\t\tfoo\n" +
+		"H5\t\tfoo\n"
+
+	want := voicebank.PrefixMap{
+		midi.Note(midi.C(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.Db(5)): voicebank.Prefix{"", "foo"},
+		//midi.Note(midi.Db(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.D(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.Eb(5)): voicebank.Prefix{"", "foo"},
+		//midi.Note(midi.Eb(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.E(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.F(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.Gb(5)): voicebank.Prefix{"", "foo"},
+		//midi.Note(midi.Gb(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.G(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.Ab(5)): voicebank.Prefix{"", "foo"},
+		//midi.Note(midi.Ab(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.A(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.Bb(5)): voicebank.Prefix{"", "foo"},
+		//midi.Note(midi.Bb(5)):  voicebank.Prefix{"", "foo"},
+		midi.Note(midi.B(5)): voicebank.Prefix{"", "foo"},
+	}
+
+	prefixmap, err := voicebank.DecodePrefixMap(strings.NewReader(s))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, want, prefixmap)
+}
+
+func TestDecodePrefixMapInvalidLine(t *testing.T) {
+	s := "C#5\tfoo\n"
+
+	prefixmap, err := voicebank.DecodePrefixMap(strings.NewReader(s))
+	assert.Nil(t, prefixmap)
+	assert.ErrorContains(t, err, "voicebank prefix.map: invalid line")
+}
+
+func TestDecodePrefixMapInvalidNote(t *testing.T) {
+	s := "XX\t\tfoo\n"
+
+	prefixmap, err := voicebank.DecodePrefixMap(strings.NewReader(s))
+	assert.Nil(t, prefixmap)
+	assert.ErrorContains(t, err, "invalid note format")
+}
