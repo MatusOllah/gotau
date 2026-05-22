e36f73597cf521fa3b236f2f1992a57e8ee480fb
SladkyCitron <57183251+SladkyCitron@users.noreply.github.com>
2026-05-22 12:03:01 +0200

    Port Japanese VCV to new API

--- phonemizer/japanese_vcv.go
+++ phonemizer/japanese_vcv.go
@@ -2,95 +2,47 @@ package phonemizer
 
 import (
 	"iter"
-	"strings"
-	"unicode/utf8"
+	"slices"
 
 	"golang.org/x/text/unicode/norm"
-
-	"github.com/SladkyCitron/gotau/voicebank"
 )
+
+// loosely based on OpenUtau
+// https://github.com/stakira/OpenUtau/blob/master/OpenUtau.Plugin.Builtin/JapaneseVCVPhonemizer.cs
 
 var _ Phonemizer = (*JapaneseVCV)(nil)
 
 // JapaneseVCV is a Japanese vowel+consonant+vowel (VCV) [Phonemizer].
 //
 // It extracts the vowel from the final kana of the previous lyric.
-//
-// [JapaneseVCV] emits candidates based on the following order:
-//
-//  1. prefix.map + lyric with VCV prefix (if [JapaneseVCV.PrefixMap] and [ResolveConfig.PrevLyric] are present)
-//  2. whitespace-trimmed lyric with VCV prefix (if [ResolveConfig.PrevLyric] is present)
-//  3. raw lyric with VCV prefix (if [ResolveConfig.PrevLyric] is present)
-type JapaneseVCV struct {
-	// PrefixMap contains the prefix.map rules for note-based prefix / suffix lookup.
-	// Optional.
-	PrefixMap voicebank.PrefixMap
-}
+type JapaneseVCV struct{}
 
-// Resolve satisfies the [Phonemizer] interface.
-func (p *JapaneseVCV) Resolve(cfg ResolveConfig) iter.Seq[string] {
-	return func(yield func(string) bool) {
-		prev := norm.NFC.String(cfg.PrevLyric)
-		lyric := norm.NFC.String(cfg.Lyric)
+// Phonemize satisfies the [Phonemizer] interface.
+func (p *JapaneseVCV) Phonemize(notes []Note, prev *Note, _ *Note) iter.Seq[Phoneme] {
+	return func(yield func(Phoneme) bool) {
+		combos := make([]string, 0, 4)
 
-		vowel := ""
-		if cfg.PrevLyric != "" {
-			vowel = getLastKanaVowel(prev)
+		note := notes[0]
+		lyric := norm.NFC.String(note.Lyric)
+
+		//TODO: phonetic hints??
+
+		// aliases for when there's no previous note
+		combos = append(combos, "- "+lyric, lyric)
+
+		// previous note
+		if prev != nil {
+			prevLyric := norm.NFC.String(prev.Lyric)
+			vowel := kanaTailVowel(prevLyric)
+			combos = combos[:0]
+			combos = append(combos,
+				string(vowel)+" "+lyric,
+				"* "+lyric,
+				lyric,
+				"- "+lyric,
+			)
 		}
 
-		vcvPrefix := "- "
-		if vowel != "" {
-			vcvPrefix = vowel + " "
-		}
-
-		vcvCombo := vcvPrefix + lyric
-
-		// prefix.map
-		if entry, ok := p.PrefixMap[cfg.Note]; ok {
-			if !yield(entry.Prefix + vcvCombo + entry.Suffix) {
-				return
-			}
-		}
-
-		// trimmed lyric
-		if !yield(vcvPrefix + strings.TrimSpace(lyric)) {
-			return
-		}
-
-		// raw lyric
-		// no need to check yield result; this is the final candidate
-		yield(vcvCombo)
-	}
-}
-
-func getLastKanaVowel(lyric string) string {
-	r, _ := utf8.DecodeLastRuneInString(lyric)
-	switch r {
-	case 'a', 'A',
-		'ぁ', 'あ', 'か', 'が', 'さ', 'ざ', 'た', 'だ', 'な', 'は', 'ば', 'ぱ', 'ま', 'ゃ', 'や', 'ら', 'わ',
-		'ァ', 'ア', 'カ', 'ガ', 'サ', 'ザ', 'タ', 'ダ', 'ナ', 'ハ', 'バ', 'パ', 'マ', 'ャ', 'ヤ', 'ラ', 'ワ':
-		return "a"
-	case 'i', 'I',
-		'ぃ', 'い', 'き', 'ぎ', 'し', 'じ', 'ち', 'ぢ', 'に', 'ひ', 'び', 'ぴ', 'み', 'り', 'ゐ',
-		'ィ', 'イ', 'キ', 'ギ', 'シ', 'ジ', 'チ', 'ヂ', 'ニ', 'ヒ', 'ビ', 'ピ', 'ミ', 'リ', 'ヰ':
-		return "i"
-	case 'u', 'U',
-		'ぅ', 'う', 'く', 'ぐ', 'す', 'ず', 'つ', 'づ', 'ぬ', 'ふ', 'ぶ', 'ぷ', 'む', 'ゅ', 'ゆ', 'る',
-		'ゥ', 'ウ', 'ク', 'グ', 'ス', 'ズ', 'ツ', 'ヅ', 'ヌ', 'フ', 'ブ', 'プ', 'ム', 'ュ', 'ユ', 'ル', 'ヴ':
-		return "u"
-	case 'e', 'E',
-		'ぇ', 'え', 'け', 'げ', 'せ', 'ぜ', 'て', 'で', 'ね', 'へ', 'べ', 'ぺ', 'め', 'れ', 'ゑ',
-		'ェ', 'エ', 'ケ', 'ゲ', 'セ', 'ゼ', 'テ', 'デ', 'ネ', 'ヘ', 'ベ', 'ペ', 'メ', 'レ', 'ヱ':
-		return "e"
-	case 'o', 'O',
-		'ぉ', 'お', 'こ', 'ご', 'そ', 'ぞ', 'と', 'ど', 'の', 'ほ', 'ぼ', 'ぽ', 'も', 'ょ', 'よ', 'ろ', 'を',
-		'ォ', 'オ', 'コ', 'ゴ', 'ソ', 'ゾ', 'ト', 'ド', 'ノ', 'ホ', 'ボ', 'ポ', 'モ', 'ョ', 'ヨ', 'ロ', 'ヲ':
-		return "o"
-	case 'n', 'N', 'ん', 'ン':
-		return "n"
-	case 'ー':
-		return getLastKanaVowel(lyric[:len(lyric)-1])
-	default:
-		return ""
+		yield(Phoneme{Index: 0, Candidates: slices.Compact(combos)})
 	}
 }
--- phonemizer/japanese_vcv_test.go
+++ phonemizer/japanese_vcv_test.go
@@ -5,39 +5,31 @@ import (
 	"testing"
 
 	"github.com/SladkyCitron/gotau/phonemizer"
-	"github.com/SladkyCitron/gotau/voicebank"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestJapaneseVCV(t *testing.T) {
-	p := &phonemizer.JapaneseVCV{PrefixMap: voicebank.PrefixMap{60: voicebank.Prefix{"pre", "suf"}}}
-	got := slices.Collect(p.Resolve(phonemizer.ResolveConfig{
-		Lyric: "か ",
-		Note:  60,
-	}))
+	p := &phonemizer.JapaneseVCV{}
+	got := slices.Collect(p.Phonemize([]phonemizer.Note{{Lyric: "か "}}, nil, nil))
 
-	want := []string{
-		"pre- か suf", // prefix.map
-		"- か",        // trimmed
-		"- か ",       // raw
-	}
+	want := []phonemizer.Phoneme{{Index: 0, Candidates: []string{
+		"- か ", // no prev
+		"か ",   // lyric
+	}}}
 
 	assert.Equal(t, want, got)
 }
 
 func TestJapaneseVCV_PrevLyric(t *testing.T) {
-	p := &phonemizer.JapaneseVCV{PrefixMap: voicebank.PrefixMap{60: voicebank.Prefix{"pre", "suf"}}}
-	got := slices.Collect(p.Resolve(phonemizer.ResolveConfig{
-		PrevLyric: "ミ",
-		Lyric:     "ク ",
-		Note:      60,
-	}))
+	p := &phonemizer.JapaneseVCV{}
+	got := slices.Collect(p.Phonemize([]phonemizer.Note{{Lyric: "ク"}}, &phonemizer.Note{Lyric: "ミ"}, nil))
 
-	want := []string{
-		"prei ク suf", // prefix.map
-		"i ク",        // trimmed
-		"i ク ",       // raw
-	}
+	want := []phonemizer.Phoneme{{Index: 0, Candidates: []string{
+		"i ク",
+		"* ク",
+		"ク",
+		"- ク",
+	}}}
 
 	assert.Equal(t, want, got)
 }
