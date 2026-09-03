a4885704be53aeca89abae60ee5de39befb64507
SladkyCitron <57183251+SladkyCitron@users.noreply.github.com>
2026-09-03 10:51:21 +0200

    refactor(observance): remove IsTetoBirthday function

--- internal/observance/observance.go
+++ internal/observance/observance.go
@@ -11,8 +11,3 @@ func IsMikuDay(t time.Time) bool {
 func IsMikuBirthday(t time.Time) bool {
 	return t.Month() == time.August && t.Day() == 31
 }
-
-// IsTetoBirthday checks if the given time is Kasane Teto's birthday (April 1st i.e. April Fools' Day).
-func IsTetoBirthday(t time.Time) bool {
-	return t.Month() == time.April && t.Day() == 1
-}
